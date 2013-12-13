3859101a70f31a91f0c884d8085c4cbd95ed4559
Les Orchard <me@lmorchard.com>
2013-12-13 17:19:10 -0500

    First stab at using Backgrid with some market orders

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -11,5 +11,35 @@ $(document).ready(function () {
             CCPEVE.showInfo(el.data('typeid'), el.data('itemid'));
             return false;
         });
+    
+    $('#gridplay').each(function () {
+        
+        var MarketOrder = Backbone.Model.extend({});
+        
+        var MarketOrders = Backbone.Collection.extend({
+            model: MarketOrder,
+            url: "/orders.json?type=sell"
+        });
+
+        var orders = new MarketOrders();
+
+        var columns = [
+            { name: 'typeName', label: 'Item', cell: 'string' },
+            { name: 'price', label: 'Price', cell: 'number' },
+            { name: 'volRemaining', label: 'Vol. Entered', cell: 'number' },
+            { name: 'volEntered', label: 'Vol. Remaining', cell: 'number' },
+            { name: 'stationName', label: 'Station', cell: 'string' }
+        ];
+
+        var grid = new Backgrid.Grid({
+            columns: columns,
+            collection: orders
+        });
+
+        $('#gridplay').append(grid.render().$el);
+
+        orders.fetch({reset: true});
+    
+    });
 
 });
