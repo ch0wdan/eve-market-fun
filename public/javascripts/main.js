9b9dcac39cee2a24600eb609295942fc6118ebd2
Les Orchard <me@lmorchard.com>
2013-12-15 01:15:51 -0500

    Playing with market group browsing

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,70 @@
 $(document).ready(function () {
 
+    var EventHub = _.extend({}, Backbone.Events);
+
+    var ShowInfoCell = Backgrid.Cell.extend({
+        className: "showinfo-cell",
+        typeID: '3867',
+        itemIDAttr: 'itemID',
+        render: function () {
+            this.$el.empty();
+            var rawValue = this.model.get(this.column.get("name"));
+            var formattedValue = this.formatter.fromRaw(rawValue, this.model);
+            var itemID = this.model.get(this.itemIDAttr);
+            this.$el.append($("<a>", {
+                class: 'showInfo',
+                href: '',
+                "data-typeID": this.typeID,
+                "data-itemID": itemID
+            }).text(formattedValue));
+            this.delegateEvents();
+            return this;
+        }
+    });
+
+    var ShowMarketDetailsCell = Backgrid.Cell.extend({
+        className: "showmarketdetails-cell",
+        typeIDAttr: 'typeID',
+        render: function () {
+            this.$el.empty();
+            var rawValue = this.model.get(this.column.get("name"));
+            var formattedValue = this.formatter.fromRaw(rawValue, this.model);
+            var typeID = this.model.get(this.typeIDAttr);
+            this.$el.append(
+                $("<a>", {
+                    class: 'showMarketDetails',
+                    href: '',
+                    "data-typeID": typeID
+                })
+                .append($("<img>", {
+                    class: 'itemThumb',
+                    src: 'http://image.eveonline.com/Type/' + typeID + '_32.png',
+                    "data-itemID": typeID
+                }))
+                .append($("<span>").text(formattedValue))
+            );
+            this.delegateEvents();
+            return this;
+        }
+    });
+
+    var ProgressIntegerCell = Backgrid.Cell.extend({
+        className: "progressinteger-cell",
+        totalAttr: "volEntered",
+        render: function () {
+            this.$el.empty();
+            var rawValue = this.model.get(this.column.get("name"));
+            var formattedValue = this.formatter.fromRaw(rawValue, this.model);
+            var totalValue = this.model.get(this.totalAttr);
+            this.$el
+                .append($('<span>', {class: 'remaining'}).text(formattedValue))
+                .append($('<span>').text(' / '))
+                .append($('<span>', {class: 'total'}).text(totalValue))
+            this.delegateEvents();
+            return this;
+        }
+    });
+
     $(document)
         .delegate('a.showMarketDetails', 'click', function () {
             var el = $(this);
@@ -11,8 +76,120 @@ $(document).ready(function () {
             CCPEVE.showInfo(el.data('typeid'), el.data('itemid'));
             return false;
         });
+
+    $('#tree').each(function () {
+        var tree = $(this);
+        var base_url = '/data/invMarketGroups?shallow';
+
+        var treeFormat = function (root) {
+            return _.map(root, function (item, id) {
+                var out = {
+                    key: item.marketGroupID,
+                    title: item.marketGroupName
+                };
+                if (item.children) { out.lazy = true; }
+                return out;    
+            });
+        }
+        tree.fancytree({
+            checkbox: true,
+            selectMode: 3,
+            source: $.Deferred(function (dfd) {
+                $.getJSON(base_url, function (data) {
+                    dfd.resolve(treeFormat(data));
+                });
+            }),
+            lazyload: function (event, data) {
+                data.result = $.Deferred(function (dfd) {
+                    var node = data.node;
+                    var url = base_url + '&root=' + node.key;
+                    $.getJSON(url, function (data) {
+                        dfd.resolve(treeFormat(data[node.key].children));
+                    });
+                });
+            },
+            activate: function (event, data) {
+                var node = data.node;
+                console.log(node.title);
+            },
+            select: function (event, data) {
+                var node = data.node;
+                var selected_ids = _.chain(data.tree.getSelectedNodes())
+                    .map(function (node) { return node.key })
+                    .value();
+                EventHub.trigger('marketGroupsSelected', selected_ids);
+            },
+            dblclick: function(e, data) {
+                data.node.toggleSelected();
+            },
+            keydown: function(e, data) {
+                if( e.which === 32 ) {
+                    data.node.toggleSelected();
+                    return false;
+                }
+            }
+        });
     
-    $('#gridplay').each(function () {
+    });
+
+    $('#items').each(function () {
+        var InvType = Backbone.Model.extend({});
+        
+        var InvTypes = Backbone.Collection.extend({
+            model: InvType,
+            url: "/data/invTypes?marketGroupID=77"
+        });
+
+        var items = new InvTypes();
+
+        var columns = [
+            { name: 'typeName', label: 'Item', editable: false,
+                cell: ShowMarketDetailsCell.extend({typeIDAttr: 'typeID'}) },
+            //{ name: 'metaGroupName', label: 'Meta', editable: false, cell: 'string' },
+            { name: 'categoryName', label: 'Category', editable: false, cell: 'string' },
+            { name: 'groupName', label: 'Group', editable: false, cell: 'string' },
+            { name: 'volume', label: 'Volume', editable: false, cell: 'number' }
+        ];
+
+        var grid = new Backgrid.Grid({
+            columns: columns,
+            collection: items
+        });
+
+        $('#items').append(grid.render().$el);
+
+        items.fetch({reset: true});
+
+        EventHub.on('marketGroupsSelected', function (selected_ids) {
+            items.url = '/data/invTypes?marketGroupID=' + selected_ids.join('&marketGroupID=');
+            items.fetch({reset: true});
+        });
+    });
+
+    if (false) $('#marketGroups').each(function () {
+        var root_el = $(this);
+        $.getJSON('/data/invMarketGroups', function (data) {
+            var render = function(root_el, root_groups) {
+                _.each(root_groups.children, function (group, id) {
+                    console.log(group);
+                    var el = $('<li>')
+                        .append($('<span>')
+                            .attr('title', group.description)
+                            .text(group.marketGroupName)
+                        );
+                    root_el.append(el);
+                    if (_.keys(group.children).length > 0) {
+                        var children_el = $('<ul>');
+                        el.append(children_el);
+                        render(children_el, group);
+                    }
+                });
+            };
+            render(root_el, data);
+        });
+    });
+    
+    if (false) $('#gridplay').each(function () {
         
         var MarketOrder = Backbone.Model.extend({});
         
@@ -22,69 +199,6 @@ $(document).ready(function () {
         });
 
         var orders = new MarketOrders();
-
-        var ShowInfoCell = Backgrid.Cell.extend({
-            className: "showinfo-cell",
-            typeID: '3867',
-            itemIDAttr: 'itemID',
-            render: function () {
-                this.$el.empty();
-                var rawValue = this.model.get(this.column.get("name"));
-                var formattedValue = this.formatter.fromRaw(rawValue, this.model);
-                var itemID = this.model.get(this.itemIDAttr);
-                this.$el.append($("<a>", {
-                    class: 'showInfo',
-                    href: '',
-                    "data-typeID": this.typeID,
-                    "data-itemID": itemID
-                }).text(formattedValue));
-                this.delegateEvents();
-                return this;
-            }
-        });
-
-        var ShowMarketDetailsCell = Backgrid.Cell.extend({
-            className: "showmarketdetails-cell",
-            typeIDAttr: 'typeID',
-            render: function () {
-                this.$el.empty();
-                var rawValue = this.model.get(this.column.get("name"));
-                var formattedValue = this.formatter.fromRaw(rawValue, this.model);
-                var typeID = this.model.get(this.typeIDAttr);
-                this.$el.append(
-                    $("<a>", {
-                        class: 'showMarketDetails',
-                        href: '',
-                        "data-typeID": typeID
-                    })
-                    .append($("<img>", {
-                        class: 'itemThumb',
-                        src: 'http://image.eveonline.com/Type/' + typeID + '_32.png',
-                        "data-itemID": typeID
-                    }))
-                    .append($("<span>").text(formattedValue))
-                );
-                this.delegateEvents();
-                return this;
-            }
-        });
-
-        var ProgressIntegerCell = Backgrid.Cell.extend({
-            className: "progressinteger-cell",
-            totalAttr: "volEntered",
-            render: function () {
-                this.$el.empty();
-                var rawValue = this.model.get(this.column.get("name"));
-                var formattedValue = this.formatter.fromRaw(rawValue, this.model);
-                var totalValue = this.model.get(this.totalAttr);
-                this.$el
-                    .append($('<span>', {class: 'remaining'}).text(formattedValue))
-                    .append($('<span>').text(' / '))
-                    .append($('<span>', {class: 'total'}).text(totalValue))
-                this.delegateEvents();
-                return this;
-            }
-        });
 
         var columns = [
             { name: 'typeName', label: 'Item', editable: false,
