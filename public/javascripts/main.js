3e6ac06dfbba7131a8e40a6b15252baf81a16cdd
Les Orchard <me@lmorchard.com>
2014-01-02 02:58:07 -0500

    More work toward market orders

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -13,3 +13,89 @@ $(document).ready(function () {
         });
 
 });
+
+var ShowInfoCell = Backgrid.Cell.extend({
+    className: "showinfo-cell",
+    typeID: '3867',
+    itemIDAttr: 'itemID',
+    render: function () {
+        this.$el.empty();
+        var rawValue = this.model.get(this.column.get("name"));
+        var formattedValue = this.formatter.fromRaw(rawValue, this.model);
+        var itemID = this.model.get(this.itemIDAttr);
+        this.$el.append($("<a>", {
+            class: 'showInfo',
+            href: '',
+            "data-typeID": this.typeID,
+            "data-itemID": itemID
+        }).text(formattedValue));
+        this.delegateEvents();
+        return this;
+    }
+});
+
+var ShowMarketDetailsCell = Backgrid.Cell.extend({
+    className: "showmarketdetails-cell",
+    typeIDAttr: 'typeID',
+    render: function () {
+        this.$el.empty();
+        var rawValue = this.model.get(this.column.get("name"));
+        var formattedValue = this.formatter.fromRaw(rawValue, this.model);
+        var typeID = this.model.get(this.typeIDAttr);
+        this.$el.append(
+            $("<a>", {
+                class: 'showInfo',
+                href: "",
+                "data-typeID": this.model.get('typeID'),
+                title: this.model.get('description')
+            })
+            .append($("<img>", {
+                class: 'itemThumb',
+                src: 'http://image.eveonline.com/Type/' + typeID + '_32.png',
+                "data-typeID":  this.model.get('typeID')
+            })));
+        this.$el.append(
+            $("<a>", {
+                class: 'showMarketDetails',
+                href: '',
+                "data-typeID": typeID
+            })
+            .append($("<span>").text(formattedValue)));
+        this.delegateEvents();
+        return this;
+    }
+});
+
+var ProgressIntegerCell = Backgrid.Cell.extend({
+    className: "progressinteger-cell",
+    totalAttr: "volEntered",
+    render: function () {
+        this.$el.empty();
+        var rawValue = this.model.get(this.column.get("name"));
+        var formattedValue = this.formatter.fromRaw(rawValue, this.model);
+        var totalValue = this.model.get(this.totalAttr);
+        this.$el
+            .append($('<span>', {class: 'remaining'}).text(formattedValue))
+            .append($('<span>').text(' / '))
+            .append($('<span>', {class: 'total'}).text(totalValue))
+        this.delegateEvents();
+        return this;
+    }
+});
+
+var NamedIntegerCell = Backgrid.IntegerCell.extend({
+    className: "metalevel-cell",
+    nameAttr: "metaGroupName",
+    render: function () {
+        this.$el.empty();
+        var rawValue = this.model.get(this.column.get("name"));
+        var formattedValue = this.formatter.fromRaw(rawValue, this.model);
+        var nameValue = this.model.get(this.nameAttr);
+        var out = (nameValue) ? nameValue + ' / ' : '';
+        out += 'Meta ' + formattedValue;
+        this.$el.append($('<span>', {class: 'name', title: rawValue})
+                        .text(out));
+        this.delegateEvents();
+        return this;
+    }
+});
