141d6054d99410d3cfb7a1561e8d79c9c2aea2a0
Les Orchard <me@lmorchard.com>
2014-03-22 14:05:46 -0400

    Store history as JSON blob rather than individual rows

---
+++ migrations/20140322135421_market_history_tweaks.js
@@ -0,0 +1,39 @@
+var _ = require('underscore');
+var Promise = require('bluebird');
+
+exports.up = function (knex) {
+    var ks = knex.schema;
+    return Promise.all([
+        ks.table('MarketHistoryAggregates', function (t) {
+            t.text('history', 'longtext');
+            t.dropColumn('profitByVolume');
+            t.dropColumn('profitByVolumeForMonth');
+            t.dropColumn('profitByVolumeForWeek');
+        }),
+        ks.dropTable('MarketHistory')
+    ]);
+};
+
+exports.down = function (knex) {
+    var ks = knex.schema;
+    return Promise.all([
+        ks.table('MarketHistoryAggregates', function (t) {
+            t.decimal('profitByVolume', 19, 4);
+            t.decimal('profitByVolumeForMonth', 19, 4);
+            t.decimal('profitByVolumeForWeek', 19, 4);
+        }),
+        ks.createTable('MarketHistory', function (t) {
+            t.engine('InnoDB');
+            t.increments('id').primary();
+            t.timestamps();
+            t.bigInteger('typeID').index();
+            t.bigInteger('regionID').index();
+            t.dateTime('date').index();
+            t.integer('orders');
+            t.integer('quantity');
+            t.decimal('low', 19, 4);
+            t.decimal('high', 19, 4);
+            t.decimal('average', 19, 4);
+        })
+    ]);
+};
